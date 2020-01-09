59667bcd599c87e83bb39813b33294fece2bdc87
Jacob Butfiloski <jacobbutfiloski@gmail.com>
2020-01-09 18:27:28 -0500

    v 1.3.2 Release
    Mostly just documentation

--- TextEditor/TextEditor/Form1.Designer.cs
+++ TextEditor/TextEditor/Form1.Designer.cs
@@ -39,26 +39,26 @@
             this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.changeSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.dEFAULTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.dEFAULTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.BackColor = System.Drawing.Color.Gray;
-            this.menuStrip1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Font = new System.Drawing.Font("Minion Pro", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
             this.settingsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(836, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(836, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -69,17 +69,17 @@
             this.saveAsToolStripMenuItem,
             this.saveToolStripMenuItem,
             this.openToolStripMenuItem});
-            this.fileToolStripMenuItem.Font = new System.Drawing.Font("Nouveau IBM Stretch", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
             this.fileToolStripMenuItem.Text = "FILE";
             // 
             // newToolStripMenuItem
             // 
-            this.newToolStripMenuItem.Font = new System.Drawing.Font("Nouveau IBM Stretch", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.newToolStripMenuItem.Name = "newToolStripMenuItem";
             this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
-            this.newToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(246, 24);
             this.newToolStripMenuItem.Text = "NEW";
             this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
             // 
@@ -90,23 +90,23 @@
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
             this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
             | System.Windows.Forms.Keys.S)));
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(246, 24);
             this.saveAsToolStripMenuItem.Text = "SAVE AS";
             this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
             // 
             // toolStripTextBox1
             // 
             this.toolStripTextBox1.BackColor = System.Drawing.Color.White;
-            this.toolStripTextBox1.Font = new System.Drawing.Font("Nouveau IBM Stretch", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.toolStripTextBox1.Name = "toolStripTextBox1";
-            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 21);
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 22);
             this.toolStripTextBox1.Text = "untitled";
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
             this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(246, 24);
             this.saveToolStripMenuItem.Text = "SAVE";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
@@ -114,7 +114,7 @@
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
             this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(246, 24);
             this.openToolStripMenuItem.Text = "OPEN";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
@@ -124,24 +124,31 @@
             this.changeSettingsToolStripMenuItem,
             this.saveToolStripMenuItem1,
             this.dEFAULTToolStripMenuItem});
-            this.settingsToolStripMenuItem.Font = new System.Drawing.Font("Nouveau IBM Stretch", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.settingsToolStripMenuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
-            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(96, 24);
             this.settingsToolStripMenuItem.Text = "SETTINGS";
             // 
             // changeSettingsToolStripMenuItem
             // 
             this.changeSettingsToolStripMenuItem.Name = "changeSettingsToolStripMenuItem";
-            this.changeSettingsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.changeSettingsToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
             this.changeSettingsToolStripMenuItem.Text = "CHANGE";
             this.changeSettingsToolStripMenuItem.Click += new System.EventHandler(this.changeSettingsToolStripMenuItem_Click);
             // 
             // saveToolStripMenuItem1
             // 
             this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
-            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(145, 22);
             this.saveToolStripMenuItem1.Text = "SAVE";
             this.saveToolStripMenuItem1.Click += new System.EventHandler(this.saveToolStripMenuItem1_Click);
+            // 
+            // dEFAULTToolStripMenuItem
+            // 
+            this.dEFAULTToolStripMenuItem.Name = "dEFAULTToolStripMenuItem";
+            this.dEFAULTToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.dEFAULTToolStripMenuItem.Text = "DEFAULT";
+            this.dEFAULTToolStripMenuItem.Click += new System.EventHandler(this.DEFAULTToolStripMenuItem_Click);
             // 
             // textBox1
             // 
@@ -151,14 +158,14 @@
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
             this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Font = new System.Drawing.Font("Nouveau IBM Stretch", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(0, 24);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 28);
             this.textBox1.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.textBox1.MaxLength = 9999999;
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox1.Size = new System.Drawing.Size(836, 513);
+            this.textBox1.Size = new System.Drawing.Size(836, 509);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textBox1.WordWrap = false;
@@ -172,10 +179,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Gray;
-            this.label1.Font = new System.Drawing.Font("Nouveau IBM Stretch", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(152, 5);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.Size = new System.Drawing.Size(127, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "CHARACTERS: 0";
             // 
@@ -183,19 +190,12 @@
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Gray;
-            this.label2.Font = new System.Drawing.Font("Nouveau IBM Stretch", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(278, 5);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.Size = new System.Drawing.Size(128, 16);
             this.label2.TabIndex = 3;
             this.label2.Text = "WORD COUNT: 0";
-            // 
-            // dEFAULTToolStripMenuItem
-            // 
-            this.dEFAULTToolStripMenuItem.Name = "dEFAULTToolStripMenuItem";
-            this.dEFAULTToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.dEFAULTToolStripMenuItem.Text = "DEFAULT";
-            this.dEFAULTToolStripMenuItem.Click += new System.EventHandler(this.DEFAULTToolStripMenuItem_Click);
             // 
             // Form1
             // 
@@ -213,6 +213,7 @@
             this.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.Name = "Form1";
             this.Text = "BEITEN";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
--- TextEditor/TextEditor/Form1.cs
+++ TextEditor/TextEditor/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,28 +16,45 @@ namespace TextEditor
 {
     public partial class Form1 : Form
     {
+        //Caret Variables
+        [DllImport("user32.dll")]
+        static extern bool CreateCaret(IntPtr hWnd, IntPtr hBitmap, int nWidth, int nHeight);
+        [DllImport("user32.dll")]
+        static extern bool ShowCaret(IntPtr hWnd);
+
+        //Is a file currently open?
         bool fileOpen = false;
+        //Current open file's path
         string fileName = "";
-        string filePath = "";
-
+        //Path of the folder
         string folderPath = "";
-
+        //Is the settings currently open?
         bool settingsOpen = false;
+        //Private Font Collection to allow form to display font without user needing to install
+        PrivateFontCollection pfc = new PrivateFontCollection();
         public Form1()
         {
             InitializeComponent();
             
             
-
+            //Read settings to set style
             var sr = new StreamReader(System.IO.Path.GetDirectoryName(Application.ExecutablePath) + @"/resources/settings.txt");
-            textBox1.Font = new Font(textBox1.Font.FontFamily, float.Parse(sr.ReadLine()));
+            //Set a fontsize variable for later from the settings file
+            var fontSize = float.Parse(sr.ReadLine());
+            //Set a Color variable for text color
             Color textColor = Color.FromName(sr.ReadLine());
+            //Assign previous color variable to textbox font
             textBox1.ForeColor = textColor;
+            //Change the menu text color
             menuStrip1.ForeColor = Color.Black;
+            //Assign the menu background color
             menuStrip1.BackColor = Color.Gray;
+            //Assign ARGB color for the main background
             textBox1.BackColor = Color.FromArgb(01, 00, 128);
             this.BackColor = Color.FromArgb(01, 00, 128);
+            //Custom renderer used for selection color
             menuStrip1.Renderer = new myRenderer();
+            //Used for alignment
             var text = sr.ReadLine();
             switch(text)
             {
@@ -49,12 +68,38 @@ namespace TextEditor
                     textBox1.TextAlign = HorizontalAlignment.Center;
                     break;
             }
+            //Dispose stream leader
             sr.Dispose();
+            //Add embedded file to private font family
+            pfc.AddFontFile(Path.GetDirectoryName(Application.ExecutablePath) + @"\Nouveau_IBM_Stretch.ttf");
+
+            //Apply custom font to controls (probably sloppy dont know any alternatives)
+            textBox1.Font = new Font(pfc.Families[0], fontSize, FontStyle.Regular);
+            fileToolStripMenuItem.Font = new Font(pfc.Families[0], 12, FontStyle.Regular);
+            settingsToolStripMenuItem.Font = new Font(pfc.Families[0], 12, FontStyle.Regular);
+            newToolStripMenuItem.Font = new Font(pfc.Families[0], 12, FontStyle.Regular);
+            saveAsToolStripMenuItem.Font = new Font(pfc.Families[0], 12, FontStyle.Regular);
+            saveToolStripMenuItem.Font = new Font(pfc.Families[0], 12, FontStyle.Regular);
+            openToolStripMenuItem.Font = new Font(pfc.Families[0], 12, FontStyle.Regular);
+            changeSettingsToolStripMenuItem.Font = new Font(pfc.Families[0], 12, FontStyle.Regular);
+            saveToolStripMenuItem1.Font = new Font(pfc.Families[0], 12, FontStyle.Regular);
+            dEFAULTToolStripMenuItem.Font = new Font(pfc.Families[0], 12, FontStyle.Regular);
+            label1.Font = new Font(pfc.Families[0], 12, FontStyle.Regular);
+            label2.Font = new Font(pfc.Families[0], 12, FontStyle.Regular);
+
             
+
         }
 
+        /// <summary>
+        /// Triggered when "Save As" is clicked
+        /// Intended to allow for user to select save location
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            //Pretty simple, just using a folderBrowserDialog to browse for a location, then saving the file there
             if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
             {
                 try
@@ -85,15 +130,26 @@ namespace TextEditor
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Triggered when "Open" is clicked
+        /// Intention is to allow users to browse for and open text files
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            //Uses an openFileDialog control to navigate to a file
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 try
                 {
+                    //Search for file
                     var sr = new StreamReader(openFileDialog1.FileName);
+                    //Assign text to the textbox
                     textBox1.Text = sr.ReadToEnd();
+                    //Add the path to the title
                     this.Text = "BEITEN " + openFileDialog1.FileName;
+                    //Let the program know a file is open currently
                     fileOpen = true;
                     fileName = openFileDialog1.FileName;
                     sr.Dispose();
@@ -106,8 +162,15 @@ namespace TextEditor
             }
         }
 
+        /// <summary>
+        /// Triggered when "Save" (from File option) is clicked
+        /// Intention is to allow document to be saved
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            //Simply writing to a previously opened txt file
             if(fileOpen)
             {
                 try
@@ -124,17 +187,31 @@ namespace TextEditor
             }
         }
 
+        /// <summary>
+        /// Triggered when "Change Settings" is clicked
+        /// Intention is to allow user to customize the editor.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void changeSettingsToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            //Let the program know the settings are currently open
             settingsOpen = true;
             saveToolStripMenuItem_Click(sender, e);
             textBox1.Text = @"Font:" + Environment.NewLine + textBox1.Font.Size + Environment.NewLine + "Color:" + Environment.NewLine + textBox1.ForeColor.Name + Environment.NewLine + "Align:" + Environment.NewLine + textBox1.TextAlign;
         }
-
+        /// <summary>
+        /// Triggered when "Save"(In settings option) is clicked
+        /// Intention is for the user to save their custom settings
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void saveToolStripMenuItem1_Click(object sender, EventArgs e)
         {
+            //Check if settings is open
             if(settingsOpen)
             {
+                //Assign variables based on the settings
                 string size = textBox1.Lines[1];
                 string color = textBox1.Lines[3];
                 string text = textBox1.Lines[5];
@@ -142,6 +219,7 @@ namespace TextEditor
 
                 try
                 {
+                    //Edit alignment
                     switch (text)
                     {
                         case "LEFT":
@@ -154,9 +232,13 @@ namespace TextEditor
                             textBox1.TextAlign = HorizontalAlignment.Center;
                             break;
                     }
-                    textBox1.Font = new Font(textBox1.Font.FontFamily, float.Parse(size));
+                    //Edit font size
+                    textBox1.Font = new Font(pfc.Families[0], float.Parse(size));
+                    //Edit font color
                     textBox1.ForeColor = Color.FromName(color);
+                    //Let the program know settings is no longer open
                     settingsOpen = false;
+                    //Check if a file is currently active and then go back to where the user was
                     if(fileOpen)
                     {
                         var sr = new StreamReader(fileName);
@@ -165,7 +247,7 @@ namespace TextEditor
                         fileOpen = true;
                         sr.Dispose();
                     }
-
+                    //Write user settings to its file
                     System.IO.StreamWriter writer = new StreamWriter(System.IO.Path.GetDirectoryName(Application.ExecutablePath) + @"/resources/settings.txt");
                     writer.Write(textBox1.Lines[1] + Environment.NewLine + textBox1.Lines[3] + Environment.NewLine + textBox1.Lines[5] + Environment.NewLine);
                     writer.Close();
@@ -178,28 +260,42 @@ namespace TextEditor
                 }
             }
         }
-
+        /// <summary>
+        /// Triggered when "New" is clicked
+        /// Intention is to give the user something to start off with when creating a document.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void newToolStripMenuItem_Click(object sender, EventArgs e)
         {
             textBox1.Text =
             "//////////////////////////////////////////////////////////////////////////////////////////" + Environment.NewLine +
             "//////////////////////////////////////////////////////////////////////////////////////////" + Environment.NewLine +
-            "///BEITEN v.1.1.0                                                                      ///" + Environment.NewLine +
+            "///BEITEN v.1.3.2                                                                      ///" + Environment.NewLine +
             "///                                                                                    ///" + Environment.NewLine +
-            "///                                  --UNTITLED DOCUMENT--                             ///" + Environment.NewLine +
+            "///                                  --UNTITLED DOC--                                  ///" + Environment.NewLine +
             "///                                                                                    ///" + Environment.NewLine +
             "///                                                                                    ///" + Environment.NewLine +
             "///BY JAKE BUTFILOSKI                                                                  ///" + Environment.NewLine +
             "//////////////////////////////////////////////////////////////////////////////////////////" + Environment.NewLine +
             "//////////////////////////////////////////////////////////////////////////////////////////" + Environment.NewLine;
         }
-
+        /// <summary>
+        /// Updates the word count and character count whenever a change is made in the textbox.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TextBox1_TextChanged(object sender, EventArgs e)
         {
             label1.Text = "CHARACTERS: " + textBox1.Text.Count();
             label2.Text = "WORD COUNT: " + CountWords(textBox1.Text);
         }
 
+        /// <summary>
+        /// Counts words for previous method
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
         public static int CountWords(string a)
         {
             int count = 0;
@@ -220,10 +316,16 @@ namespace TextEditor
             return count;
         }
 
+        /// <summary>
+        /// Triggered when "Default" is clicked
+        /// Intention is to restore user settings to how it was shipped
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void DEFAULTToolStripMenuItem_Click(object sender, EventArgs e)
         {
             var sr = new StreamReader(System.IO.Path.GetDirectoryName(Application.ExecutablePath) + @"/resources/default.txt");
-            textBox1.Font = new Font(textBox1.Font.FontFamily, float.Parse(sr.ReadLine()));
+            textBox1.Font = new Font(pfc.Families[0], float.Parse(sr.ReadLine()));
             Color textColor = Color.FromName(sr.ReadLine());
             textBox1.ForeColor = textColor;
             menuStrip1.ForeColor = Color.Black;
@@ -245,7 +347,22 @@ namespace TextEditor
                     break;
             }
         }
+
+        /// <summary>
+        /// Used to create the caret
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            CreateCaret(textBox1.Handle, IntPtr.Zero, 11, textBox1.Font.Height);
+            ShowCaret(textBox1.Handle);
+            
+        }
     }
+    /// <summary>
+    /// Soley for changing the color of selection
+    /// </summary>
     class myRenderer : ToolStripProfessionalRenderer
     {
         protected override void OnRenderMenuItemBackground(ToolStripItemRenderEventArgs myMenu)
